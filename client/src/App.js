140ab6fa77bf80df751d72f87fb85522a45f5a08
francisca hernandez <francisca.hernandez17@gmail.com>
2022-12-13 01:29:48 -0800

    created a basic navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+//import react from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
+
+function Example(args) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar {...args}>
+        <NavbarBrand href="/">Hidden Gems</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="">
+                GitHub
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Homepage</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Example;
