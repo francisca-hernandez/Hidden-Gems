27698fefc2ef4ba14cb487ac1f92818abf7b4d7b
“Gigi <debortolg@gmail.com>
2022-12-17 14:45:36 -0800

    adding notes to file for FH to continue work

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,16 +1,32 @@
+import React from 'react';
+// import Footer from '../components/Footer';
+// import Navbar from '../components/Navbar';
 // needs a logout function just on the dashboard 
 import { useQuery } from '@apollo/client';
 import { QUERY_ME } from '../utils/queries';
-// import Auth from '../utils/auth';
+import Auth from '../utils/auth';
 
-// linking the saveGems     
+// linking the saveGems
 // jsx - same as the other pages
 // declaring variable and inputing stuff that needs to be imported
 
 const Dashboard = () => {
     const { loading, data } = useQuery(QUERY_ME)
     console.log(data); 
-}
+
+    const savedGems = data?.savedGems || [];
+
+
+    const loggedIn = Auth.loggedIn();
+
+    return (
+        <main>
+            <div>
+                <h1>GEMS</h1>
+            </div>
+        </main>
+    );
+};
 
 
 export default Dashboard; 
