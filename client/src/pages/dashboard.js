f8c6432855dd282ad20d5f97bce13bf31e1274e3
“Gigi <debortolg@gmail.com>
2022-12-17 14:29:13 -0800

    first commit - to get into repo

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -3,7 +3,7 @@ import { useQuery } from '@apollo/client';
 import { QUERY_ME } from '../utils/queries';
 // import Auth from '../utils/auth';
 
-// linking the saveGems
+// linking the saveGems     
 // jsx - same as the other pages
 // declaring variable and inputing stuff that needs to be imported
 
