fca6cf06ec3568db16517f4f89e6c27ca3d3e262
“Gigi <debortolg@gmail.com>
2022-12-19 17:36:54 -0800

    push 2

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
-import AddGem from '../components/AddGem';
-import Gems from '../Gems/index'
+// import Gems from '../components/Gems';
+import Gemsform from './Gemsform';
 // needs a logout function just on the dashboard
-import { useQuery, useMutation } from '@apollo/client';
+import { useQuery, } from '@apollo/client';
 import { QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
+
 // linking the saveGems
 // jsx - same as the other pages
 // declaring variable and inputing stuff that needs to be imported
@@ -17,21 +18,43 @@ const Dashboard = () => {
     console.log(gems);
 
     // To make sure user is logged in
-    // const loggedIn = Auth.loggedIn();
+    const loggedIn = Auth.loggedIn();
     
     return (
-    <main>
-        <Navbar></Navbar>
-        <div>
-            <Gems></Gems>
-        </div>
-        <div>
-            <AddGem></AddGem>
-        </div>
-        <Footer></Footer>
-    </main>
-    ) 
-}
+            <main>
+                <Navbar />
+
+                <section>
+                    <Gemsform />
+                </section>
+                {/* <div className="container-fluid"></div>
+              <div className="flex-row justify-space-between">
+                {loggedIn && (
+                  <div className="col-12 mb-3">
+                    <Gemsform />
+                  </div>
+                )}
+                <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
+                  {loading ? (
+                    <div>Loading...</div>
+                  ) : (
+                    <Gems 
+                      gems={gems}
+                      title="Found Treasures"
+                    />
+                  )}
+                </div>
+                {loggedIn && Gems? (
+                  <div className="col-12 col-lg-3 mb-3">
+                    <Gems/>
+                  </div>
+                ) : null}
+              </div> */}
+              <Footer />
+            </main>
+          );
+        };
+
 export default Dashboard;
 // data.me.savedGems is the query call - may need a keyword
 // data.me.savedGems will need to be a map function OR we can use another type of loop
